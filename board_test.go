c69e9cb3615862d8fde738295f63c4a16859f7cd
Felipe Sere <felipesere@gmail.com>
2018-12-18 12:32:42 +0000

    Use a beforeEach to stream line tests and reduce repetition

--- board_test.go
+++ board_test.go
@@ -6,16 +6,21 @@ import (
 )
 
 var _ = Describe("a board", func() {
-	It("has 9 possible moves", func() {
-		anEmptyBoard := NewBoard(3)
 
+	var (
+		anEmptyBoard Board
+	)
+
+	BeforeEach(func() {
+		anEmptyBoard = NewBoard(3)
+	})
+
+	It("has 9 possible moves", func() {
 		numberOfPossibleMoves := len(anEmptyBoard.GetPossibleMoves())
 		Expect(numberOfPossibleMoves).To(Equal(9))
 	})
 
 	It("has fewer possible moves when moves are made", func() {
-		anEmptyBoard := NewBoard(3)
-
 		possibleMoves := anEmptyBoard.GetPossibleMoves()
 
 		updatedBoard, _ := anEmptyBoard.MakeMove(possibleMoves[0], X)
@@ -26,8 +31,6 @@ var _ = Describe("a board", func() {
 	})
 
 	It("cant mark the same position twice", func() {
-		anEmptyBoard := NewBoard(3)
-
 		updatedBoard, _ := anEmptyBoard.MakeMove(0, X)
 		updatedBoard, err := updatedBoard.MakeMove(0, X)
 
@@ -35,16 +38,12 @@ var _ = Describe("a board", func() {
 	})
 
 	It("cant mark outside of the bounds of the board", func() {
-		anEmptyBoard := NewBoard(3)
-
-		anEmptyBoard, err := anEmptyBoard.MakeMove(12, X)
+		_, err := anEmptyBoard.MakeMove(12, X)
 
 		Expect(err).To(HaveOccurred())
 	})
 
 	It("finds a winner in the first row", func() {
-		anEmptyBoard := NewBoard(3)
-
 		first, _ := anEmptyBoard.MakeMove(0, O)
 		second, _ := first.MakeMove(1, O)
 		third, _ := second.MakeMove(2, O)
@@ -55,8 +54,6 @@ var _ = Describe("a board", func() {
 	})
 
 	It("finds a winner in the first column", func() {
-		anEmptyBoard := NewBoard(3)
-
 		first, _ := anEmptyBoard.MakeMove(0, X)
 		second, _ := first.MakeMove(3, X)
 		third, _ := second.MakeMove(6, X)
@@ -67,8 +64,6 @@ var _ = Describe("a board", func() {
 	})
 
 	It("finds a winner in the first diagonal", func() {
-		anEmptyBoard := NewBoard(3)
-
 		first, _ := anEmptyBoard.MakeMove(0, X)
 		second, _ := first.MakeMove(4, X)
 		third, _ := second.MakeMove(8, X)
@@ -79,8 +74,6 @@ var _ = Describe("a board", func() {
 	})
 
 	It("finds a winner in the second diagonal", func() {
-		anEmptyBoard := NewBoard(3)
-
 		first, _ := anEmptyBoard.MakeMove(2, X)
 		second, _ := first.MakeMove(4, X)
 		third, _ := second.MakeMove(6, X)
